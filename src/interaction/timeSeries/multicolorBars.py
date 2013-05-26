455a600bbb23344231963f4cfb1e4bad00fd4abd
Tylar <murray.tylar@gmail.com>
2013-05-26 00:35:46 -0400

    added separate data class for viewLog data

--- src/interaction/timeSeries/multicolorBars.py
+++ src/interaction/timeSeries/multicolorBars.py
@@ -28,76 +28,85 @@ def activePassiveMap(tag):
 	else:
 		return 0.5
 
+class data:
+	def __init__(self):
+		self.rowCount = 0
+		self.count = 0
+
+		self.x = list()
+		self.p1 = list()
+		self.p2 = list()
+		self.p3 = list()
+		self.a1 = list()
+		self.a2 = list()
+		self.a3 = list()
+		self.sl = list()
+		self.ER = list()
+
+	def getData(self, viewFileLoc):
+		import csv
+		# read in csv
+		loadingDisplay=""	# this is a simple string to print so you know it's working
+		updateFreq = 2000	# how many items per display update?
+		div = 1000*60	# to reduce the amount of data (we don't really need millisecond-accurate readings)
+
+		print "loading", viewFileLoc
+		with open(viewFileLoc, 'rb') as csvfile:
+			spamreader = csv.reader(csvfile, delimiter=',')
+
+			for row in spamreader:
+				if self.rowCount==0:	#skip header row
+					self.rowCount+=1
+					continue
+				if self.rowCount==1:	# set startTime
+					startTime = int(int(row[0])/div)
+				self.rowCount+=1
+				# print ', '.join(row)	# print the raw data
+				# print row		# print raw data matrix
+				for time in range(int(int(row[0])/div),int(int(row[1])/div)):	# for time span
+				#	t = time-startTime	#time from start of study
+				#	hrs = t/60/60
+				#	x.append(hrs)	# x is time in hrs
+					self.x.append(datetime.datetime.fromtimestamp(time*60))	
+					self.p1.append(0)
+					self.p2.append(0)
+					self.p3.append(0)
+					self.a1.append(0)
+					self.a2.append(0)
+					self.a3.append(0)
+					self.sl.append(0)
+					self.ER.append(0)
+
+					tag = row[3]
+					# passives are negative
+					if tag == 'onComputer':
+						self.p1[-1] = -1
+					elif tag == 'videoGames':
+						self.p2[-1] = -1
+					elif tag == 'watchingTV':
+						self.p3[-1] = -1
+					# actives are positive
+					elif tag == 'bicycling':
+						self.a1[-1] = 1
+					elif tag == 'basketball':
+						self.a2[-1] = 1
+					elif tag == 'running':
+						self.a3[-1] = 1
+					elif tag == 'inBed':
+						self.sl[-1] = (-0.5)
+					else:	#ERROR
+						self.ER[-1] = (0.5)
+				#	y.append(float(activePassiveMap(row[3])))
+					self.count+=1
+					if self.count % updateFreq == 0:
+						loadingDisplay+="|"
+						print loadingDisplay
+		print 'done. '+str(self.count)+' datapoints loaded from '+str(self.rowCount)+' rows.'
+
 def plot():
-	print "loading", viewFileLoc
+	viewData = data()
+	viewData.getData(viewFileLoc)
 
-	# read in csv
-	loadingDisplay=""	# this is a simple string to print so you know it's working
-	updateFreq = 2000	# how many items per display update?
-	rowCount = 0
-	count = 0
-
-	div = 1000*60	# to reduce the amount of data (we don't really need millisecond-accurate readings)
-	import csv
-	with open(viewFileLoc, 'rb') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',')
-		x = list()
-		p1 = list()
-		p2 = list()
-		p3 = list()
-		a1 = list()
-		a2 = list()
-		a3 = list()
-		sl = list()
-		ER = list()
-		for row in spamreader:
-			if rowCount==0:	#skip header row
-				rowCount+=1
-				continue
-			if rowCount==1:	# set startTime
-				startTime = int(int(row[0])/div)
-			rowCount+=1
-			# print ', '.join(row)	# print the raw data
-			# print row		# print raw data matrix
-			for time in range(int(int(row[0])/div),int(int(row[1])/div)):	# for time span
-			#	t = time-startTime	#time from start of study
-			#	hrs = t/60/60
-			#	x.append(hrs)	# x is time in hrs
-				x.append(datetime.datetime.fromtimestamp(time*60))	
-				p1.append(0)
-				p2.append(0)
-				p3.append(0)
-				a1.append(0)
-				a2.append(0)
-				a3.append(0)
-				sl.append(0)
-				ER.append(0)
-
-				tag = row[3]
-				# passives are negative
-				if tag == 'onComputer':
-					p1[-1] = -1
-				elif tag == 'videoGames':
-					p2[-1] = -1
-				elif tag == 'watchingTV':
-					p3[-1] = -1
-				# actives are positive
-				elif tag == 'bicycling':
-					a1[-1] = 1
-				elif tag == 'basketball':
-					a2[-1] = 1
-				elif tag == 'running':
-					a3[-1] = 1
-				elif tag == 'inBed':
-					sl[-1] = (-0.5)
-				else:	#ERROR
-					ER[-1] = (0.5)
-			#	y.append(float(activePassiveMap(row[3])))
-				count+=1
-				if count % updateFreq == 0:
-					loadingDisplay+="|"
-					print loadingDisplay
-	print 'done. '+str(count)+' datapoints loaded from '+str(rowCount)+' rows.'
 	print 'making plots...'
 	pylab.figure('multicolorBars')
 	#p = pylab.figure()
@@ -105,21 +114,21 @@ def plot():
 	# lineGraph.plot(x,y)
 	#barGraph = p.add_subplot(111)
 	#TODO: maybe these shouldn't stack??? actually, it shouldn't be a problem anyway, since they should never be concurrent...
-	pl1 = pylab.plt.bar(x,p1,linewidth=0,color='b')
-	base = p1
-	pl2 = pylab.plt.bar(x,p2,linewidth=0,bottom=base,color='oliveDrab')
-	base = [base[i] + p2[i] for i in range(len(base))]
-	pl3 = pylab.plt.bar(x,p3,linewidth=0,bottom=base,color='indigo')
-	base = [base[i] + p3[i] for i in range(len(base))]
-	pl4 = pylab.plt.bar(x,a1,linewidth=0,bottom=base,color='r')
-	base = [base[i] + a1[i] for i in range(len(base))]
-	pl5 = pylab.plt.bar(x,a2,linewidth=0,bottom=base,color='orange')
-	base = [base[i] + a2[i] for i in range(len(base))]
-	pl6 = pylab.plt.bar(x,a1,linewidth=0,bottom=base,color='orangeRed')
-	base = [base[i] + a3[i] for i in range(len(base))]
-	pl7 = pylab.plt.bar(x,sl,linewidth=0,bottom=base,color='g')
-	base = [base[i] + sl[i] for i in range(len(base))]
-	pl8 = pylab.plt.bar(x,ER,linewidth=0,bottom=base,color='deepPink')
+	pl1 = pylab.plt.bar(viewData.x,viewData.p1,linewidth=0,color='b')
+	base = viewData.p1
+	pl2 = pylab.plt.bar(viewData.x,viewData.p2,linewidth=0,bottom=base,color='oliveDrab')
+	base = [base[i] + viewData.p2[i] for i in range(len(base))]
+	pl3 = pylab.plt.bar(viewData.x,viewData.p3,linewidth=0,bottom=base,color='indigo')
+	base = [base[i] + viewData.p3[i] for i in range(len(base))]
+	pl4 = pylab.plt.bar(viewData.x,viewData.a1,linewidth=0,bottom=base,color='r')
+	base = [base[i] + viewData.a1[i] for i in range(len(base))]
+	pl5 = pylab.plt.bar(viewData.x,viewData.a2,linewidth=0,bottom=base,color='orange')
+	base = [base[i] + viewData.a2[i] for i in range(len(base))]
+	pl6 = pylab.plt.bar(viewData.x,viewData.a1,linewidth=0,bottom=base,color='orangeRed')
+	base = [base[i] + viewData.a3[i] for i in range(len(base))]
+	pl7 = pylab.plt.bar(viewData.x,viewData.sl,linewidth=0,bottom=base,color='g')
+	base = [base[i] + viewData.sl[i] for i in range(len(base))]
+	pl8 = pylab.plt.bar(viewData.x,viewData.ER,linewidth=0,bottom=base,color='deepPink')
 
 	pylab.plt.xlabel('time [hrs]')
 	pylab.plt.legend( (pl1[0]      ,pl2[0]      ,pl3[0]      ,pl4[0]     ,pl5[0]      ,pl6[0]   ,pl7[0] ,pl8[0]),\
