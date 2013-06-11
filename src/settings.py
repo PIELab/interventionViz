105f662d799d2bca7c27df14dcc39ef86c5e1892
7yl4r <murray.tylar@gmail.com>
2013-06-11 12:31:51 -0400

    scaling option added to preferences

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,12 @@
 dataLocation = "./data/"	# location of participant folders
 
-pid          = "mockParticipant"# participant folder name
+#pid          = "mockParticipant"# participant folder name
+#viewLogFile = "dataLog.txt"	#name of view log file
+#PAfile      = "miles/DAILY_TOTALS_358344041300005_2012-05-18.txt"	#name of physical activity data file
 
+pid          = "1"# participant folder name
 viewLogFile = "dataLog.txt"	#name of view log file
-PAfile      = "miles/DAILY_TOTALS_358344041300005_2012-05-18.txt"	#name of physical activity data file
+PAfile      = "mMonitor/miles/DAILY_TOTALS_358344041300005_2013-05-23.txt"	#name of physical activity data file
 
 viewFileLoc = dataLocation+pid+'/'+viewLogFile;
 PAfileLoc   = dataLocation+pid+'/'+PAfile;
--- testAll.py
+++ testAll.py
@@ -6,8 +6,12 @@ import pylab
 import src.interaction.timeSeries.multicolorBars
 src.interaction.timeSeries.multicolorBars.plot()
 
+pylab.plt.show()
+
 import src.PA.timeSeries.dailyMinutes
 src.PA.timeSeries.dailyMinutes.plot()
+
+pylab.plt.show()
 
 import src.interaction_x_PA.scatterPlot
 src.interaction_x_PA.scatterPlot.plot()
