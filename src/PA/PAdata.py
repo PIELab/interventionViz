080c222d84e9bbed0251b79f4a020753b418f951
7yl4r <murray.tylar@gmail.com>
2014-03-10 13:31:02 -0400

    added methods for getting minute-level fitbit data

--- src/PA/PAdata.py
+++ src/PA/PAdata.py
@@ -9,7 +9,11 @@ DEFAULT_TIMESCALE = 'daily'
 
 class PAdata:
 	def __init__(self,PAfile=None):
+		self.loaded = False
+	
 		self.time = list()
+		
+		self.steps = list()
 
 		self.nonWear = list()
 		self.sedentary = list()
@@ -23,17 +27,35 @@ class PAdata:
 		self.count = 0
 		
 		if PAfile != None:
+			self.sourceFile = PAfile
 			self.getData(PAfile)
+			self.loaded = True 
+	
+	def reset():
+		''' 
+		clears all data in the object and resets counters
+		'''
+		self = PAdata(self.sourceFile)
 
 	# results in a list of values corresponding to days in the study. Dates of the days are included in 'self.time'.
 	def getData(self, PAfileLoc, method=DEFAULT_METHOD, timeScale=DEFAULT_TIMESCALE):
+		if self.loaded == True:
+			self.reset()
+			
 		if timeScale == 'daily':
 			self.getDailyData(PAfileLoc,method)
-		elif timeScale == 'raw':
-			#lowest time scale available
-			raise noImplementedError('raw timescale data getter not implemented')
+		elif timeScale == 'minute':
+			self.getMinuteLevelData(PAfileLoc,method)
 		else:
 			raise valueError('data timescale "'+str(timeScale)+'" not recognized')
+			
+	### PRIVATE METHODS ###
+			
+	def getMinuteLevelData(self,PAfileLoc,method=DEFAULT_METHOD):
+		if method=='fitbit':
+			self.getMinuteLevelFitbitData(PAfileLoc)
+		elif method=='mMonitor':
+			raise noImplementedError('minute timescale data getter for mMonitor not yet implemented')
 
 	def getDailyData(self, PAfileLoc, method=DEFAULT_METHOD):
 		if method=='fitbit':
@@ -43,10 +65,21 @@ class PAdata:
 		else:
 			raise valueError('data getter method "'+str(method)+'" not recognized')
 		
-
+	def getMinuteLevelFitbitData(self,PAfileLoc):
+		with open(PAfileLoc, 'rb') as csvfile:
+			spamreader = csv.reader(csvfile,delimter=',')
+			for row in spamreader:
+				if self.rowCount==0: # skip header row
+					self.rowCount+=1
+					continue
+				self.rowCount+=1
+				time = datetime.strptime(row[0]), "%x %I %p"
+				for min in range(0,59):
+					self.time.append(time+timedelta(min=1))
+					self.steps.append(row[1+min])
+					
 	def getDailymMonitorData(self, PAfileLoc):
 		print "loading", PAfileLoc
-		
 		# read in csv
 		loadingDisplay=""	# this is a simple string to print so you know it's working
 		updateFreq = 10	# how many items per display update?
