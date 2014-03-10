bbebf21e9e68879614d05d2f6cde9473d35efd37
7yl4r <murray.tylar@gmail.com>
2014-03-10 12:59:33 -0400

    added additional options for data loading to choose mMonitor or fitbit and daily vs minute-level timescale

--- src/PA/PAdata.py
+++ src/PA/PAdata.py
@@ -4,6 +4,9 @@ import dateutil.parser	#for parsing datestrings
 import csv		#for csv file reading
 from datetime import datetime
 
+DEFAULT_METHOD = 'mMonitor'
+DEFAULT_TIMESCALE = 'daily'
+
 class PAdata:
 	def __init__(self,PAfile=None):
 		self.time = list()
@@ -23,9 +26,27 @@ class PAdata:
 			self.getData(PAfile)
 
 	# results in a list of values corresponding to days in the study. Dates of the days are included in 'self.time'.
-	def getData(self, PAfileLoc):
-		print "loading", PAfileLoc
+	def getData(self, PAfileLoc, method=DEFAULT_METHOD, timeScale=DEFAULT_TIMESCALE):
+		if timeScale == 'daily':
+			self.getDailyData(PAfileLoc,method)
+		elif timeScale == 'raw':
+			#lowest time scale available
+			raise noImplementedError('raw timescale data getter not implemented')
+		else:
+			raise valueError('data timescale "'+str(timeScale)+'" not recognized')
 
+	def getDailyData(self, PAfileLoc, method=DEFAULT_METHOD):
+		if method=='fitbit':
+			raise noImplementedError('daily fitbit data getter not implemented')
+		elif method=='mMonitor':
+			self.getDailymMonitorData(PAfileLoc)
+		else:
+			raise valueError('data getter method "'+str(method)+'" not recognized')
+		
+
+	def getDailymMonitorData(self, PAfileLoc):
+		print "loading", PAfileLoc
+		
 		# read in csv
 		loadingDisplay=""	# this is a simple string to print so you know it's working
 		updateFreq = 10	# how many items per display update?
